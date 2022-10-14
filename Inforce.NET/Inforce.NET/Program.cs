078600d3e422786f91810006ad00e415c8b0747d
unknown <daniilpoyarkov@gmail.com>
2022-10-14 14:57:05 +0300

    Test

--- Inforce.NET/Inforce.NET/Program.cs
+++ Inforce.NET/Inforce.NET/Program.cs
@@ -1,6 +1,6 @@
 using Inforce.NET;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); 
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
